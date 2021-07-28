eab006c92555d6158e5a1deceefa6f46f8d28e1c
DamianoLandi <damianolandi01@gmail.com>
2021-07-28 17:23:08 +0200

    Inserimento Immagini

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const root = new Vue({
+    el: "#root",
+    data: {
+        imgs:[
+            "./images/image1.jpg", 
+            "./images/image2.jpg", 
+            "./images/image3.jpg", 
+            "./images/image4.jpg"
+        ]
+    },
+    methods: {
+
+    },
+});
